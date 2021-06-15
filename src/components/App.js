46c48e35d923df214a519d55cd1cbb001ced0e25
Jonathan García <jona03g97@gmail.com>
2021-06-15 09:56:35 -0600

    [Repaso React]: Stateful vs Stateless

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 
-const App = () => {
-  const ponerFilas = () => [
-    <tr>
-      <td>Jonathan</td>
-      <td>jona@gmail.com</td>
-      <td>jona.com</td>
-    </tr>,
-    <tr>
-      <td>Jonathan</td>
-      <td>jona@gmail.com</td>
-      <td>jona.com</td>
-    </tr>,
-  ];
-  return (
-    <div className="margen">
-      <table className="tabla">
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Correo</th>
-            <th>Enlace</th>
-          </tr>
-        </thead>
-        <tbody>{ponerFilas()}</tbody>
-      </table>
-    </div>
-  );
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      usuarios: [
+        {
+          nombre: "Jona",
+          correo: "jona@gmail.com",
+          enlace: "jona.com",
+        },
+        {
+          nombre: "Platzi",
+          correo: "self@platzi.com",
+          enlace: "platzi.com",
+        },
+      ],
+    };
+  }
+  ponerFilas = () =>
+    this.state.usuarios.map((usuario) => (
+      <tr>
+        <td>{usuario.nombre}</td>
+        <td>{usuario.correo}</td>
+        <td>{usuario.enlace}</td>
+      </tr>
+    ));
+
+  render() {
+    return (
+      <div className="margen">
+        <table className="tabla">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Correo</th>
+              <th>Enlace</th>
+            </tr>
+          </thead>
+          <tbody>{this.ponerFilas()}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 export default App;
