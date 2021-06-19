75f1b7c8e78d8b9bb84251fb66543c573eb203fa
Jonathan García <megabyte9703@gmail.com>
2021-06-19 15:42:42 -0600

    [Fases de Redux]: Conexión a un componente

--- src/components/Usuarios/index.jsx
+++ src/components/Usuarios/index.jsx
@@ -1,25 +1,19 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { connect } from "react-redux";
 
 class Usuarios extends Component {
-  constructor() {
-    super();
-    this.state = {
-      usuarios: [],
-    };
-  }
-
-  async componentDidMount() {
-    const respuesta = await axios.get(
-      "https://jsonplaceholder.typicode.com/users"
-    );
-    this.setState({
-      usuarios: respuesta.data,
-    });
-  }
+  // async componentDidMount() {
+  //   const respuesta = await axios.get(
+  //     "https://jsonplaceholder.typicode.com/users"
+  //   );
+  //   this.setState({
+  //     usuarios: respuesta.data,
+  //   });
+  // }
 
   ponerFilas = () =>
-    this.state.usuarios.map((usuario) => (
+    this.props.usuarios.map((usuario) => (
       <tr key={usuario.id}>
         <td>{usuario.name}</td>
         <td>{usuario.email}</td>
@@ -28,7 +22,7 @@ class Usuarios extends Component {
     ));
 
   render() {
-    console.log(this.state.usuarios);
+    console.log(this.props);
     return (
       <div>
         <table className="tabla">
@@ -46,4 +40,10 @@ class Usuarios extends Component {
   }
 }
 
-export default Usuarios;
+const mapStateToProps = (state) => {
+  return state.usuariosReducer;
+};
+
+export default connect(mapStateToProps, {
+  /*ACTIONS*/
+})(Usuarios);
