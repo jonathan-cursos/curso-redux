d90efc52f18cc39b030c05fe9bcf789fd2572a85
Jonathan García <megabyte9703@gmail.com>
2021-06-20 09:34:22 -0600

    [Fases extra]: Tabla como componente

--- src/components/Usuarios/index.jsx
+++ src/components/Usuarios/index.jsx
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import * as usuariosActions from "../../actions/usuariosActions";
 import Spinner from "../General/Spinner";
 import Fatal from "../General/Fatal";
+import Tabla from "./Tabla";
 
 class Usuarios extends Component {
   componentDidMount() {
@@ -18,32 +19,18 @@ class Usuarios extends Component {
       return <Fatal mensaje={this.props.error} />;
     }
 
-    return (
-      <table className="tabla">
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Correo</th>
-            <th>Enlace</th>
-          </tr>
-        </thead>
-        <tbody>{this.ponerFilas()}</tbody>
-      </table>
-    );
+    return <Tabla />;
   };
-  ponerFilas = () =>
-    this.props.usuarios.map((usuario) => (
-      <tr key={usuario.id}>
-        <td>{usuario.name}</td>
-        <td>{usuario.email}</td>
-        <td>{usuario.website}</td>
-      </tr>
-    ));
 
   render() {
     console.log(this.props.cargando);
     console.log(this.props.error);
-    return <div>{this.ponerContenido()}</div>;
+    return (
+      <div>
+        <h1>Usuarios</h1>
+        {this.ponerContenido()}
+      </div>
+    );
   }
 }
 
