fc1eb7fac6a551dad1b06571c77954a569e6bc3f
Jonathan García <megabyte9703@gmail.com>
2021-06-20 08:04:27 -0600

    [Fases extra]: Spinner componente

--- src/components/Usuarios/index.jsx
+++ src/components/Usuarios/index.jsx
@@ -1,12 +1,31 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as usuariosActions from "../../actions/usuariosActions";
+import Spinner from "../General/Spinner";
 
 class Usuarios extends Component {
   componentDidMount() {
     this.props.traerTodos();
   }
 
+  ponerContenido = () => {
+    if (this.props.cargando) {
+      return <Spinner />;
+    }
+
+    return (
+      <table className="tabla">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Enlace</th>
+          </tr>
+        </thead>
+        <tbody>{this.ponerFilas()}</tbody>
+      </table>
+    );
+  };
   ponerFilas = () =>
     this.props.usuarios.map((usuario) => (
       <tr key={usuario.id}>
@@ -19,20 +38,7 @@ class Usuarios extends Component {
   render() {
     console.log(this.props.cargando);
     console.log(this.props.error);
-    return (
-      <div>
-        <table className="tabla">
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Correo</th>
-              <th>Enlace</th>
-            </tr>
-          </thead>
-          <tbody>{this.ponerFilas()}</tbody>
-        </table>
-      </div>
-    );
+    return <div>{this.ponerContenido()}</div>;
   }
 }
 
