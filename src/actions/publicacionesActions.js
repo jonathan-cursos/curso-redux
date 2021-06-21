5983c4ae77bff10d6c03290306fe16f9e35f3671
Jonathan García <jona03g97@gmail.com>
2021-06-21 15:29:23 -0600

    [Compartir informacion en Redux]: Evitar segundas búsquedas

--- src/actions/publicacionesActions.js
+++ src/actions/publicacionesActions.js
@@ -1,35 +1,22 @@
 import axios from "axios";
-import { TRAER_TODOS, CARGANDO, ERROR } from "../types/publicacionesTypes";
-
-export const traerTodos = () => async (dispatch) => {
-  dispatch({
-    type: CARGANDO,
-  });
-  try {
-    const respuesta = await axios.get(
-      "http://jsonplaceholder.typicode.com/posts"
-    );
-    dispatch({
-      type: TRAER_TODOS,
-      payload: respuesta.data,
-    });
-  } catch (error) {
-    console.log(error.message);
-    dispatch({
-      type: ERROR,
-      payload: "Algo salió mal. Intentelo más tarde.",
-    });
-  }
-};
+import {
+  TRAER_TODOS,
+  TRAER_POR_USUARIO,
+  CARGANDO,
+  ERROR,
+} from "../types/publicacionesTypes";
 
 export const traerPorUsuario = (key) => async (dispatch, getState) => {
   const { usuarios } = getState().usuariosReducer;
+  const { publicaciones } = getState().publicacionesReducer;
   const usuario_id = usuarios[key].id;
-  const response = await axios.get(
+
+  const respuesta = await axios.get(
     `http://jsonplaceholder.typicode.com/posts?userId=${usuario_id}`
   );
+  const publicaciones_actualizadas = [...publicaciones, respuesta.data];
   dispatch({
-    type: TRAER_TODOS,
-    payload: response.data,
+    type: TRAER_POR_USUARIO,
+    payload: publicaciones_actualizadas,
   });
 };
