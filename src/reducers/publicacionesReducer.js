96508138b1488335b011159a1f693cabbb5d6e94
Jonathan García <jona03g97@gmail.com>
2021-06-24 12:59:05 -0600

    [Compartir informacion en Redux]: Props por herencia vs estado

--- src/reducers/publicacionesReducer.js
+++ src/reducers/publicacionesReducer.js
@@ -1,9 +1,17 @@
-import { ACTUALIZAR, CARGANDO, ERROR } from "../types/publicacionesTypes";
+import {
+  ACTUALIZAR,
+  CARGANDO,
+  ERROR,
+  COM_ERROR,
+  COM_CARGANDO,
+} from "../types/publicacionesTypes";
 
 const INITIAL_STATE = {
   publicaciones: [],
   cargando: false,
   error: "",
+  com_cargando: false,
+  com_error: "",
 };
 
 const publicacionesReducer = (state = INITIAL_STATE, action) => {
@@ -19,6 +27,10 @@ const publicacionesReducer = (state = INITIAL_STATE, action) => {
       return { ...state, cargando: true };
     case ERROR:
       return { ...state, error: action.payload, cargando: false };
+    case COM_CARGANDO:
+      return { ...state, com_cargando: true };
+    case COM_ERROR:
+      return { ...state, com_error: action.payload, com_cargando: false };
     default:
       return state;
   }
