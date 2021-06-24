5179770807d7142f701dd2fe7186a4e565767917
Jonathan García <jona03g97@gmail.com>
2021-06-24 15:41:58 -0600

    [Metodos HTTP]: Nuevo ciclo Redux

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,14 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Menu from "./Menu";
 import Usuarios from "./Usuarios";
 import Publicaciones from "./Publicaciones";
+import Tareas from "./Tareas";
 
-const Prueba = () => <div>hola</div>;
 const App = () => (
   <BrowserRouter>
     <Menu />
     <div id="margen">
       <Route exact path="/" component={Usuarios} />
-      <Route exact path="/tareas" component={Prueba} />
+      <Route exact path="/tareas" component={Tareas} />
       <Route exact path="/publicaciones/:key" component={Publicaciones} />
     </div>
   </BrowserRouter>
